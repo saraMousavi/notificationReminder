7dfc91dcf62bd806adb61c8bdf331726043c9bf6
ASUS <sara.mousavi.90@gmail.com>
2020-11-23 08:49:43 +0330

    first commit

---
+++ app/src/main/java/ir/sara/mousavi/reminderwithnotification/MainActivity.java
@@ -0,0 +1,14 @@
+package ir.sara.mousavi.reminderwithnotification;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
